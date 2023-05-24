3987e0bd5e97702f9cef67b0bee5c104eced910c
Loreta <badalyan.loreta@gmail.com>
2023-05-24 22:03:08 +0400

    added some comments in html and js

--- script/index.js
+++ script/index.js
@@ -3,12 +3,12 @@ function activateStep(stepNumber) {
     let steps = document.getElementsByClassName('step');
     let buttons = document.getElementsByClassName('active');
   
-  
+    //for removing the "active" class from all buttons
     for (let i = 0; i < buttons.length; i++) {
       buttons[i].classList.remove('active');
     }
   
-  
+    //hide all steps
     for (let i = 0; i < steps.length; i++) {
       steps[i].style.display = 'none';
     }
@@ -16,14 +16,15 @@ function activateStep(stepNumber) {
     let selectedStep = document.getElementById('step' + stepNumber);
     selectedStep.style.display = 'block';
   
-  
+    
     let selectedButton = document.getElementById('step' + stepNumber + 'button');
+    // Add the "active" class to the selected button
     selectedButton.classList.add('active');
   }
   
   //Activate Step 1 by default
   activateStep(1);
-  
+
     //------------------------ menu ----------------
 
 
